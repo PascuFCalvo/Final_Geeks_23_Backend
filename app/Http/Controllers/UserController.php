adef26526cfbb1088fb7ea8a8943abc2d41d97e9
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-12-07 17:35:39 +0100

    feat:register a brand works

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Brand;
 use App\Models\User;
 use App\Models\Streamer;
 use Illuminate\Http\Request;
@@ -44,6 +44,52 @@ class UserController extends Controller
                     "success" => true,
                     "message" => "streamer registered",
                     "data" => $newStreamer
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error registering user"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+    public function registerBrand(Request $request)
+    {
+        try {
+            $newUser = User::create(
+                [
+                    'user_name' => $request->user_name,
+                    'user_email' => $request->user_email,
+                    'password' => $request->password,
+                    'user_phone' => $request->user_phone,
+                    'user_role' => 'brand',
+                    'is_active' => true,
+                    'user_avatar_link' => $request->user_avatar_link,
+                ]
+            );
+
+            $newBrand = Brand::create(
+                [
+                    'user_id' => $newUser->id,
+                    'brand_name' => $request->brand_name,
+                    'brand_cif' => $request->brand_cif,
+                    'brand_description' => $request->brand_description,
+                    'brand_logo_link' => $request->brand_logo_link,
+                    'country_id' => $request->country_id,
+                    'has_active_campaigns' => false,
+                ]
+            );
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "brand registered",
+                    "data" => $newBrand
                 ],
                 Response::HTTP_CREATED
             );
--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -6,4 +6,18 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Brand extends Authenticatable
 {
+
+   protected $fillable = [
+
+
+      'user_id',
+      'brand_name',
+      'brand_cif',
+      'brand_description',
+      'brand_logo_link',
+      'country_id',
+      'has_active_campaigns',
+
+
+   ];
 }
